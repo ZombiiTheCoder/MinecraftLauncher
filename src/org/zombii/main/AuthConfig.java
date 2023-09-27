830103253ff867cb0fc873d8e93fa42fd20a74f2
Zombi <zombeifedgaming@outlook.com>
2023-09-26 20:16:36 -0500

    add Authentification Possibilites using AuthConfig.json

---
+++ src/org/zombii/main/AuthConfig.java
@@ -0,0 +1,10 @@
+package org.zombii.main;
+
+public class AuthConfig {
+    public String name;
+    public String uuid;
+    public String xuid;
+    public String clientID;
+    public String accessToken;
+    public String userType;
+}
---
+++ src/org/zombii/main/MainAuth.java
@@ -0,0 +1,64 @@
+package org.zombii.main;
+
+import org.zombii.launcher.FabricLauncher;
+import org.zombii.launcher.QuiltLauncher;
+import org.zombii.launcher.VanillaLauncher;
+
+public class MainAuth {
+    public static configParser config;
+
+    public static configParser init() throws Exception {
+        config.loadVmanifest();
+        config.GetVersionTypeAndUrl();
+        return config;
+    }
+
+    public static void main(String[] args) throws Exception {
+        config = new configParser();
+        config.loadConfig();
+        config.loadAuthConfig();
+
+        switch (config.config.launcher) {
+            case "Fabric":
+                FabricLauncher fabric = new FabricLauncher(config);
+                fabric.useAuthConfig(true);
+                if (fabric.VersionInstalled()) {
+                    fabric.Launch();
+                } else {
+                    fabric.Install();
+                    fabric.Launch();
+                }
+                break;
+
+            case "Quilt":
+                QuiltLauncher quilt = new QuiltLauncher(config);
+                quilt.useAuthConfig(true);
+                if (quilt.VersionInstalled()) {
+                    quilt.Launch();
+                } else {
+                    quilt.Install();
+                    quilt.Launch();
+                }
+                break;
+
+            // case "Forge":
+            // ForgeLauncher forge = new ForgeLauncher(config);
+            // if (forge.VersionInstalled() ) { forge.Launch(); }
+            // else { forge.Install(); forge.Launch(); } break;
+
+            case "Vanilla":
+                VanillaLauncher vanilla = new VanillaLauncher(init());
+                vanilla.useAuthConfig(true);
+                vanilla.AlterMainClass(false);
+                vanilla.UseCustomLaunchWrapper(false);
+                if (vanilla.VersionInstalled()) {
+                    vanilla.Launch();
+                } else {
+                    vanilla.Install();
+                    vanilla.Launch();
+                }
+                break;
+        }
+    }
+
+}
